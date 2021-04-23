a292161d682d68d3294f294cc0996bf6bbad90e4
Bryant Garcia <bryantgarcia@BCs-MacBook-Pro-2.local>
2021-04-23 10:27:37 -0500

    Functionality for Timer and Categories component complete. Initial Version of project complete.

--- src/letter/Letter.js
+++ src/letter/Letter.js
@@ -1,28 +1,26 @@
 import './Letter.css'
-import { useState } from "react";
+import { useState } from 'react'
 
 const Letter = () => {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    const changeLetter = () => {
-      const randomIndex = Math.floor(Math.random() * letters.length);
-    }
+  const changeLetter = () => {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    setIndex(randomIndex);
+  }
   
-
-    return(
-        <div className='Letter'>
-            <div className='Letter-Top'>
-                <p>LETTER</p>
-                <button onClick={changeLetter}
-                 className='Letter-Top-Btn'>Roll Again</button>
-            </div>
-            <div className='Letter-Display'>
-            <h1 className='Letter-Display-H1'>{letters[index]}</h1>
-            </div>
-        </div>
-    );
+  return(
+    <div className='Letter'>
+      <div className='Letter-Top'>
+        <p>LETTER</p>
+        <button onClick={changeLetter} className='Letter-Top-Btn'>Roll Again</button>
+      </div>
+      <div className='Letter-Display'>
+        <h1 className='Letter-Display-H1'>{letters[index]}</h1>
+      </div>
+    </div>
+  );
 }
-
 export default Letter;
